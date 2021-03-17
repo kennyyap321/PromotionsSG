1d446353101a6a40f81db30d58607f1e4f40fa26
Junhan Feng <junhan.feng@ncs.com.sg>
2021-03-18 01:40:52 +0800

    test push

--- LoginAPI/Program.cs
+++ LoginAPI/Program.cs
@@ -16,6 +16,7 @@ namespace Common.FileUpload
     {
         //public static readonly string Namespace = typeof(Program).Namespace;
         //public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+        //test push - jh
 
         public static void Main(string[] args)
         {
