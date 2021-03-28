ae9199840aebf6923fbea45665ac776c78ca9df6
Junhan Feng <junhan.feng@ncs.com.sg>
2021-03-28 23:30:57 +0800

    Added back prior changes for LoginController

--- PromotionsSG.API/PromotionsSG.API.Login/Program.cs
+++ PromotionsSG.API/PromotionsSG.API.Login/Program.cs
@@ -1,26 +1,28 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PromotionsSG.API.Login
 {
     public class Program
     {
+        //public static readonly string Namespace = typeof(Program).Namespace;
+        //public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+        .ConfigureLogging(logging =>
+        {
+            // When you need logging below set the minimum level. Otherwise the logging framework will default to Informational for external providers.
+            logging.SetMinimumLevel(LogLevel.Debug);
+        })
+        .UseStartup<Startup>();
+
     }
 }
