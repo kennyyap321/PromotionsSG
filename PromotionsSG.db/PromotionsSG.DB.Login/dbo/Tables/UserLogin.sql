ef1c374d9f678ef9a1223c990a1a0414e532f0a0
Junhan Feng <junhan.feng@ncs.com.sg>
2021-04-03 03:23:50 +0800

    Added for login module - controllers - sql
    Updated appsettings connection string.
    -Please create new db user 'sgdev/test1234' to standardize login across all machines

---
+++ PromotionsSG.db/PromotionsSG.DB.Login/dbo/Tables/UserLogin.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserLogin]
+(
+	[UserId]       VARCHAR (50)  NOT NULL,
+    [UserLoginId]    VARCHAR (100) NOT NULL,
+    [UserName]     VARCHAR (250) NOT NULL,
+    [Password] VARCHAR (500) NOT NULL,
+    CONSTRAINT [PK_UserLogin] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
