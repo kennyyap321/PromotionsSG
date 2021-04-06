590e727ea67a449dd89d929138ee242c7a7b3859
nazurahsamad <nazurah.bintesamad@ncs.com.sg>
2021-04-06 23:34:20 +0800

    Updated table with new columns for CustomerProfile module

--- PromotionsSG.db/PromotionsSG.DB.CustomerProfile/dbo/Tables/CustomerProfile.sql
+++ PromotionsSG.db/PromotionsSG.DB.CustomerProfile/dbo/Tables/CustomerProfile.sql
@@ -1,7 +1,24 @@
-﻿CREATE TABLE [dbo].[CustomerProfile]
+﻿CREATE TABLE [dbo].[CustomerProfile](
+	[CustomerProfileId] [varchar](50) NOT NULL,
+	[CustomerFullName] [varchar](250) NULL,
+	[CustomerAddress] [varchar](500) NULL,
+	[CustomerEmail] [varchar](500) NULL,
+	[CustomerPhone] [varchar](50) NULL,
+	[CustomerType] [varchar](50) NULL,
+	[CustomerGender] [varchar](10) NULL,
+	[CustomerActive] [bit] NULL,
+	[CustomerDOB] [datetime] NULL,
+	[CreatedBy] [varchar](50) NULL,
+	[CreatedTime] [datetime] NULL,
+	[LastUpdatedBy] [varchar](50) NULL,
+	[LastUpdatedTime] [datetime] NULL,
+	[VersionNo] [int] NULL,
+	[IsDeleted] [bit] NULL,
+ CONSTRAINT [PK_CustomerProfile] PRIMARY KEY CLUSTERED 
 (
-	 [CustomerProfileId]         VARCHAR (50)  NOT NULL,
-    [CustomerName]              VARCHAR (255) NOT NULL,
-    [CustomerAddress]          VARCHAR (250) NOT NULL,
-    CONSTRAINT [PK_CustomerProfile] PRIMARY KEY CLUSTERED ([CustomerProfileId] ASC)
-);
+	[CustomerProfileId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
