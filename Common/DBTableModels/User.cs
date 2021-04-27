1604c9c6fb10eaf4b7cc2a0999fb368a02ddea6e
ngchongpeng <ngchongpeng@hotmail.com>
2021-04-27 17:30:22 +0800

    1. Change display name for field from username > email 2. Change display name for field from usertype > shop / customer

--- Common/DBTableModels/User.cs
+++ Common/DBTableModels/User.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Common.DBTableModels
@@ -9,9 +10,11 @@ namespace Common.DBTableModels
         public int UserId{ get; set; }
 
         [Required]
+        [DisplayName("Shop / Customer")]
         public int UserType { get; set; }
 
         [Required]
+        [DisplayName("Email")]
         public string UserName { get; set; }
 
         [Required]
